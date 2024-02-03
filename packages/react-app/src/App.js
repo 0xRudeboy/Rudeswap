81ea5cfef64bc5eefcc35d90a2536a849a41e336
0xRUDEBOY <116237237+0xRUDEBOY@users.noreply.github.com>
2024-02-03 14:51:25 +0400

    done

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,7 +1,54 @@
-import React from "react"
+import React from "react";
+import { useEthers } from "@usedapp/core";
+import styles from "./styles";
+import { uniswapLogo } from "./assets";
+import { Loader, Exchange, WalletButton } from "./components";
+
+import { usePools } from "./hooks";
 
 const App = () => {
-  return <p>Hello, Web3!</p>
-}
+  const { account } = useEthers();
+
+  const [loading, pools] = usePools();
+
+  console.log("Pools", pools);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <header className={styles.header}>
+          <img
+            src={uniswapLogo}
+            className="h-16 w-16 object-contain"
+            alt="uniswap logo"
+          />
+          <WalletButton />
+        </header>
+        <div className={styles.exchangeContainer}>
+          <h1 className={styles.headTitle}>Uniswap 2.0</h1>
+          <p className={styles.subTitle}>Exchange tokens in seconds</p>
+
+          <div className={styles.exchangeBoxWrapper}>
+            <div className={styles.exchangeBox}>
+              <div className="pink_gradient" />
+              <div className={styles.exchange}>
+                {account ? (
+                  loading ? (
+                    <Loader title="Loading pools, please wait!" />
+                  ) : (
+                    <Exchange pools={pools} />
+                  )
+                ) : (
+                  <Loader title="Please connect your wallet" />
+                )}
+              </div>
+              <div className="blue_gradient" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,13 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+// export const ROUTER_ADDRESS = "0x6E71cfbc04eF15db5CcB942f2e4C78efCDD02833";
+
+export const ROUTER_ADDRESS = "0x390eF8A23c8EeB1B9c3Fa4A270f2767Bdf5a3e86";
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]:
+      "https://eth-goerli.g.alchemy.com/v2/VFVeT3J9yUX9SZs4FStpZUZSJyIUolda",
   },
 };
